93ef7ae2924993983ae4a7e1c021bcb7f879342f
Prantho Kumer <57418052+pranthokumer@users.noreply.github.com>
2020-02-03 00:09:49 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function () {
+     $('[data-toggle="popover"]').popover();
+    
+  $('.example-popover').popover({
+    container: 'body'
+  });
+    
+$(document).ready(function(){
+  $('.toast').toast('show');
+});
+    
+ $('[data-toggle="tooltip"]').tooltip();   
+    
+})
